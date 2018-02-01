614b3487bdc1a00a5d18c48f594ea5812eab4bd2
Chinmay Ghotkar <chinmayg@vt.edu>
2018-01-31 19:55:49 -0800

    Init Commit

---
+++ Variables, Constants, and Data Types.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 32
+myAge = 33
+
+let myName : String = "Chinmay"
+let myLastName : String = "Ghotkar"
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) \(myLastName)"
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumber : Int = 12
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 3.14159265359
+
+
